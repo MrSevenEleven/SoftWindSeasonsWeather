477b37487ca822e8431db76f1635e11d79613338
wang <854468271@qq.com>
2020-02-28 16:47:39 +0800

    test
    test

---
+++ app.js
@@ -0,0 +1,9 @@
+//app.js
+App({
+  onLaunch: function (res) {
+    // console.log(res);
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ components/icon/index.js
@@ -0,0 +1,30 @@
+// components/icon.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    type:{
+      type:String,
+      value:''
+    },
+    family:{
+      type:String,
+      value:'weather'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
